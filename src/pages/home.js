b88f16e489267014c0d7c899bde0759950c97829
Luis Valgoi <luisvalgoi@gmail.com>
2022-01-13 09:57:49 -0300

    fix: mobile and background

--- src/pages/home.js
+++ src/pages/home.js
@@ -11,28 +11,23 @@ import LineTextureSrc from "../img/lines.svg";
 
 function App() {
   return (
-    <>
-      <LineTexture />
+    <LineTexture>
       <Layout>
         <Avatar />
         <Info />
         <CTAs />
         <About />
       </Layout>
-    </>
+    </LineTexture>
   );
 }
 
 const LineTexture = styled.div`
-  background-image: url(${LineTextureSrc});
+  background-image: linear-gradient(rgba(255,255,255, 0.4), rgba(255,255,255, 0.9)), url(${LineTextureSrc});
   background-repeat: no-repeat;
   background-position: center;
   background-size: cover;
   position: absolute;
-  min-width: 100%;
-  min-height: 100%;
-  transform: scale(1.2);
-  opacity: 0.4;
 `;
 
 export default App;
