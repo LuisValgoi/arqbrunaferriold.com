83d2d83419a55a4a636c272f0d58f89bb0f65e22
Luis Valgoi <luisvalgoi@gmail.com>
2022-07-04 19:26:48 -0300

    refactor: changed to env variable

--- src/components/home/cta.js
+++ src/components/home/cta.js
@@ -4,15 +4,14 @@ import tw from "tailwind-styled-components";
 import { ButtonOutline } from "../ui";
 
 const CTAs = () => {
+  const urlOrcamento = process.env.REACT_APP_CTA_ORCAMENTO;
   const urlRender = process.env.REACT_APP_CTA_RENDER;
   const urlContato = "mailto:arqbrunaferri@gmail.com";
   const urlTCC = process.env.REACT_APP_CTA_TCC_LINK;
 
-  const handleClickOrcamento = () => window.open('https://my.forms.app/arqbrunaferri/orcamento', '_blank');
-
   return (
     <Container>
-      <ButtonOutline onClick={handleClickOrcamento}>
+      <ButtonOutline onClick={() => window.open(urlOrcamento, '_blank')}>
         SOLICITAR ORÇAMENTO
       </ButtonOutline>
       <ButtonOutline onClick={() => window.open(urlRender, "_blank")}>
